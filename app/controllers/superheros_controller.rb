2deaf668a34a83681bb66300fc79cafd38ac3763
mandym_webdev <mandy@mandym.com>
2014-09-04 18:47:08 -0400

    completed: Create new-hero form using shared partial

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -7,7 +7,21 @@ class SuperherosController < ApplicationController
   def show
     @hero = Superhero.find(params[:id])
   end
-  
+
+  def create
+    @hero = Superhero.new(hero_params)
+    if @hero.save
+      redirect_to @hero
+    end
+  end
+
+  private
+  def hero_params
+    params.require(:hero).permit(:name, :cape, :super_power, :image)
+  end
 
 end
 
+
+
+  
